1f02dc8b1709d3fda7f247e3e44d151d72649d18
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2021-03-30 14:14:19 +0530

    Create Literals.java
    Literals are the constant values assigned to a variable

---
+++ Literals.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args) {
+        int age=21;
+        short age1=33;
+        long age2=333333333333L;
+        byte age3=2;
+        boolean a=true;
+        char ab='S';
+        float s=5.7f;
+        double t=8.88;
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(a);
+        System.out.println(ab);
+        System.out.println(s);
+        System.out.println(t);
+    }
+}
